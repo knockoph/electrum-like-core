82e0e869b30c4c3c01a8b27ea54ee993c11d6486
knockoph <knockoph@proton.me>
2024-11-26 21:35:44 +0100

    read_cookie

--- src/coreclient/main.cpp
+++ src/coreclient/main.cpp
@@ -1,32 +1,68 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <vector>
 #include <univalue.h>
 #include <boost/asio.hpp>
+#include <filesystem>
+#include <wordexp.h>
+
+
+std::vector<std::string> split_string(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::stringstream ss(str);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+
+std::string read_cookie() {
+    std::string rel_path = ".bitcoin/regtest/.cookie";
+    std::string home_path = getenv("HOME");
+    std::string cookie_path = home_path + "/" + rel_path;
+
+    std::ifstream file {cookie_path};
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open cookie file");
+    }
+
+    std::string line;
+    std::getline(file, line);
+
+    auto tokens = split_string(line, ':');
+    return tokens.at(1);
+}
+
+
+struct RpcSettings {
+    const std::string& host;
+    const std::string& port;
+    const std::string& cookie;
+};
 
 
 class RpcClient {
 private:
-    std::string v_host;
-    std::string v_port;
-    std::string v_auth;
+    RpcSettings settings;
     boost::asio::io_context io_context;
     boost::asio::ip::tcp::resolver resolver;
     boost::asio::ip::tcp::socket socket;
 
 public:
-    RpcClient(const std::string& host, const std::string& port, const std::string& auth):
-    v_host(host), v_port(port), v_auth(auth), resolver(io_context), socket(io_context) {}
+    RpcClient(const RpcSettings& rpc_settings): settings(rpc_settings), resolver(io_context), socket(io_context) {}
 
-    bool connect() {
+    void connect() {
         try {
-            boost::asio::connect(socket, resolver.resolve(v_host, v_port));
+            boost::asio::connect(socket, resolver.resolve(settings.host, settings.port));
         } catch (const boost::system::system_error& e) {
-            std::cerr << "Could not connect to RPC server.\n";
-            return false;
+            throw std::runtime_error("Could not connect to rpc server");
         }
-        return true;
     }
 
     std::string request(const std::string& method, const UniValue params) {
@@ -44,15 +80,13 @@ public:
 };
 
 int main() {
-    std::string host {"localhost"};
-    std::string port {"18443"};
-    std::string auth {"some_cookie"};
-
-    RpcClient client {host, port, auth};
-    if (!client.connect()) return -1;
+    auto cookie = read_cookie();
+    std::cout << cookie << "\n";
+    RpcSettings settings {"localhost", "18443", cookie};
+    RpcClient client {settings};
+    client.connect();
 
     std::vector<int> v {0, 1, 2};
-
     UniValue params {UniValue::VARR};
     for (auto e: v) {
         params.push_back(e);
