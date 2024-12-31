c5d3ff80998354010e89be35c3f4eb3db76f4d7e
knockoph <knockoph@proton.me>
2024-12-31 16:58:26 +0100

    added connection limit to example-server2

--- src/dev/example_server2.cpp
+++ src/dev/example_server2.cpp
@@ -6,24 +6,45 @@
 
 namespace asio = boost::asio;
 
+using namespace std::chrono_literals;
 
-asio::awaitable<void> handle_connection(asio::ip::tcp::socket socket, int connections) {
-    std::cout << "Handle " << connections << "\n";
-    char data[1024];
-    while (true) {
-        std::size_t n = co_await socket.async_read_some(asio::buffer(data), asio::use_awaitable);
-        std::cout << "Data " << connections << ": " << data << "\n";
-        std::string s = std::to_string(connections);
-        co_await asio::async_write(socket, asio::buffer(s.data(), s.size()), asio::use_awaitable);
+
+const int MAX_CONNECTIONS=2;
+
+
+struct ConnectionCounter {
+    int n;
+};
+
+
+asio::awaitable<void> handle_connection(asio::ip::tcp::socket socket, ConnectionCounter& cc, int id) {
+    std::cout << "Handle " << id << "\n";
+    try {
+        char data[1024];
+        while (true) {
+            std::size_t n = co_await socket.async_read_some(asio::buffer(data), asio::use_awaitable);
+            std::cout << "Data " << id << ": " << data << "\n";
+            std::string s = std::to_string(id);
+            co_await asio::async_write(socket, asio::buffer(s.data(), s.size()), asio::use_awaitable);
+        }
     }
+    catch (...) {}
+    std::cout << "Close " << id << "\n";
+    --cc.n;
 }
 
 
 asio::awaitable<void> accept_connections(asio::ip::tcp::acceptor& acceptor) {
-    int connections = 0;
+    ConnectionCounter cc {0};
+    int id = 0;
     while(true) {
         auto socket = co_await acceptor.async_accept(asio::use_awaitable);
-        asio::co_spawn(acceptor.get_executor(), handle_connection(std::move(socket), connections++), asio::detached);
+        if (cc.n >= MAX_CONNECTIONS) {
+            std::cout << "MAX CONNECTIONS REACHED\n";
+            continue;
+        }
+        ++cc.n;
+        asio::co_spawn(acceptor.get_executor(), handle_connection(std::move(socket), cc, id++), asio::detached);
     }
 }
 
@@ -32,10 +53,6 @@ int main() {
     short port {4242};
     asio::io_context io_context;
     asio::ip::tcp::acceptor acceptor (io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port));
-    //asio::ip::tcp::socket socket(io_context);
-    int max_connections {2};
-
-    //acceptor.listen(max_connections);
 
     asio::co_spawn(io_context, accept_connections(acceptor), asio::detached);
 
